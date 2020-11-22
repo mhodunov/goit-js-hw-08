5ac9998b6bf63f80be9894fedcd57efcbce027b5
Maksym Hodunov <m.godunov@yandex.ru>
2020-11-22 18:51:05 +0200

    adds closing modal window on Esc

--- script/task-1.js
+++ script/task-1.js
@@ -1,13 +1,16 @@
 import imgSrc from "./image-gallery.js";
 
 const galleryBox = document.querySelector('.js-gallery');
-const modalWindow = document.querySelector('.lightbox');
+const modalWindow = document.querySelector('.js-lightbox');
 const modalWindowImage = document.querySelector('.lightbox__image')
+const closeModalButton = document.querySelector('[data-action="close-lightbox"]')
+
 const createItems = createGalleryItem(imgSrc);
-
-
 galleryBox.insertAdjacentHTML('beforeend', createItems);
+
 galleryBox.addEventListener('click', onGalleryClick);
+closeModalButton.addEventListener('click', closeModal);
+window.addEventListener('keydown', closeModalOnEsc);
 
 function createGalleryItem(imgSrc) {
   return imgSrc
@@ -42,9 +45,20 @@ function openModal() {
 
 function closeModal() {
   modalWindow.classList.remove('is-open');
+  clearImageAttributes();
+};
+
+function closeModalOnEsc(event) {
+  if(event.code !== "Escape" || !modalWindow.classList.contains('is-open')) return;
+  closeModal();
 };
 
 function setImageAttributes(event) {
   modalWindowImage.setAttribute("src", event.target.dataset.source);
   modalWindowImage.setAttribute("alt", event.target.getAttribute("alt"));
+}
+
+function clearImageAttributes() {
+  modalWindowImage.setAttribute("src", "");
+  modalWindowImage.setAttribute("alt", "");
 }
