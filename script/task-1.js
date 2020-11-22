df23b231c37f34c2b65b2035b94d2a3346241c5e
Maksym Hodunov <m.godunov@yandex.ru>
2020-11-22 20:54:35 +0200

    Adds comments

--- script/task-1.js
+++ script/task-1.js
@@ -1,22 +1,24 @@
+//Импорт объекта с галереей
 import imgSrc from "./image-gallery.js";
 
+//Находим необходимые элементы в разметке
 const galleryBox = document.querySelector('.js-gallery');
 const modalWindow = document.querySelector('.js-lightbox');
 const modalWindowImage = document.querySelector('.lightbox__image')
 const closeModalButton = document.querySelector('[data-action="close-lightbox"]')
 
-const createItems = createGalleryItem(imgSrc);
-galleryBox.insertAdjacentHTML('beforeend', createItems);
+const createItems = createGalleryItem(imgSrc); //Создание разметки
+galleryBox.insertAdjacentHTML('beforeend', createItems); //Вставка разметки в ul.js-gallery
+const galleryItemsQty = document.querySelectorAll('.gallery__image').length; //Кол-во элементов в галерее
 
-const galleryItemsQty = document.querySelectorAll('.gallery__image').length;
-const currentImage = document.querySelector('.lightbox__image');
+//Слушатели событий
+galleryBox.addEventListener('click', onGalleryClick); //Открытие по клику на изображение
+closeModalButton.addEventListener('click', closeModal); //Закрытие по клику на кнопку
+window.addEventListener('keydown', closeModalOnEsc); //Закрытие по нажатию Esc
+window.addEventListener('keydown', carousel); //Перелистывание вправо/влево
+modalWindow.addEventListener('click', closeModalOnSideClick); //Закрытие по клику на область вокруг изображения
 
-galleryBox.addEventListener('click', onGalleryClick);
-closeModalButton.addEventListener('click', closeModal);
-window.addEventListener('keydown', closeModalOnEsc);
-window.addEventListener('keydown', carousel);
-modalWindow.addEventListener('click', closeModalOnSideClick);
-
+//Добавление разметки галереи с данными из объекта
 function createGalleryItem(imgSrc) {
   return imgSrc
   .map(({ preview, original, description }, idx) => {
@@ -36,7 +38,7 @@ function createGalleryItem(imgSrc) {
     })
   .join('');
 };
-
+//Открытие модального окна по клику на изображение
 function onGalleryClick(event) {
   event.preventDefault()
   if(event.target.nodeName !== "IMG" || modalWindow.classList.contains('is-open')) return;
@@ -44,11 +46,11 @@ function onGalleryClick(event) {
   setImageAttributes(event);
   
 }
-
+//Добавление класса .is-open при открытии модального окна
 function openModal() {
   modalWindow.classList.add('is-open');
 };
-
+//Функция для работы перелистывания вправо/влево
 function carousel(event) {
   if (event.code !== "ArrowRight" && event.code !== "ArrowLeft" || !modalWindow.classList.contains('is-open')) return;
   if (event.code === "ArrowRight") {
@@ -58,54 +60,54 @@ function carousel(event) {
     carouselBackward();
   };
 };
-
+//Перелистывание изображения вперед при нажатии стрелки
 function carouselForward() {
-  let index = +currentImage.dataset.index + 1;
+  let index = +modalWindowImage.dataset.index + 1;
   if (index > galleryItemsQty) {
     index = 1;
   }; 
   switchImage(index);
 };
-
+//Перелистывание изображения назад при нажатии стрелки
 function carouselBackward() {
-  let index = +currentImage.dataset.index - 1;
+  let index = +modalWindowImage.dataset.index - 1;
   if(index < 1) {
     index = galleryItemsQty;
   };
   switchImage(index);
 };
-
+//Закрытие модального окна
 function closeModal() {
   modalWindow.classList.remove('is-open');
   clearImageAttributes();
 };
-
+//Закрытие модального окна при нажатии на Esc
 function closeModalOnEsc(event) {
   if(event.code !== "Escape" || !modalWindow.classList.contains('is-open')) return;
   closeModal();
 };
-
+//Закрытие модального окна при клике на область вокруг изображения
 function closeModalOnSideClick(event) {
   if(event.target.nodeName === 'IMG') return;
   closeModal();
 };
-
+//Установка атрибутов изображения при открытии модального окна
 function setImageAttributes(event) {
   modalWindowImage.setAttribute("src", event.target.dataset.source);
   modalWindowImage.setAttribute("alt", event.target.getAttribute("alt"));
   modalWindowImage.setAttribute("data-index", event.target.dataset.index);
 };
-
+//Очистка атрибутов изображения после закрытия модального окна
 function clearImageAttributes() {
   modalWindowImage.setAttribute("src", "");
   modalWindowImage.setAttribute("alt", "");
   modalWindowImage.setAttribute("data-index", "");
 };
-
+//Переключние изображений при нажатии стрелок влево/вправо
 function switchImage(index) {
   const nextImage = document.querySelector(`[data-index="${index}"]`);
 
-  currentImage.setAttribute("src", nextImage.dataset.source);
-  currentImage.setAttribute("alt", nextImage.getAttribute("alt"));
-  currentImage.setAttribute("data-index", index);
+  modalWindowImage.setAttribute("src", nextImage.dataset.source);
+  modalWindowImage.setAttribute("alt", nextImage.getAttribute("alt"));
+  modalWindowImage.setAttribute("data-index", index);
 }
